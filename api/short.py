0d5559e0bf2bd7a7b05cda308e7bf8cfd8d6da5b
JuseoKo <mariat1717@daum.net>
2024-03-10 23:12:31 +0900

    feat: pytest setting add

---
+++ api/short.py
@@ -0,0 +1,29 @@
+from ..models.url_storage import URLStorage
+from fastapi import HTTPException
+import random
+import string
+
+
+def generate_short_url():
+    """랜덤한 문자열을 생성하여 단축 URL로 사용"""
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(6))
+
+
+@app.post("/shorten/")
+def shorten_url(url_request):
+    """URL을 받아서 단축 URL을 생성하고 반환"""
+    url_db = {}
+    short_url = generate_short_url()
+    url_storage = URLStorage(original_url=url_request.url, short_url=short_url)
+    url_db[short_url] = url_storage
+    return url_storage
+
+@app.get("/{short_url}")
+def redirect_to_original(short_url: str):
+    """단축 URL을 받아서 원본 URL로 리디렉션"""
+    url_db = {}
+    if short_url not in url_db:
+        raise HTTPException(status_code=404, detail="Short URL not found")
+    return {"url": url_db[short_url].original_url}
+
